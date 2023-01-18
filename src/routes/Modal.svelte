ca95afd9a01c35bbd44a8b45ce116b9b767b6ab2
salma-anika <anikaavaa@gmail.com>
2023-01-18 21:14:41 +0600

    binding set

--- src/routes/Modal.svelte
+++ src/routes/Modal.svelte
@@ -1,12 +1,97 @@
 <script>
 	import { closeModal } from 'svelte-modals';
+	
 
 	/**
 	 * @type {any}
 	 */
 	export let isOpen;
-
+    
+	import { onMount } from 'svelte';
 	
+    let selected = '2000';
+	let options = [
+		'2000',
+		'2001',
+		'2002',
+		'2003',
+		'2004',
+		'2005',
+		'2006',
+		'2007',
+		'2008',
+		'2009',
+		'2010',
+		'2011',
+		'2012',
+		'2013',
+		'2014',
+		'2015',
+		'2016',
+		'2017',
+		'2018',
+		'2019',
+		'2020',
+		'2021',
+		'2022',
+		'2023',
+		
+	]
+
+	$: console.log('Changed selected:', selected)
+	$: console.log('Updated options:', options)
+
+    let selected1 = 'Jan';
+	let options1 = [
+		'Jan',
+		'Feb',
+		'March',
+		'April',
+		'June',
+		'July',
+		'August',
+		'September',
+		'October',
+        'November',
+        'December',
+		
+	]
+
+	$: console.log('Changed selected:', selected1)
+	$: console.log('Updated options:', options1)
+
+    let selected2 = '1';
+	let options2 = [
+		'1',
+		'2',
+		'3',
+		'4',
+		'5',
+		'6',
+		'7',
+		'8',
+		'9',
+        '10',
+        '11',
+        '21',
+        '22',
+        '23',
+        '24',
+        '25',
+        '26',
+        '27',
+        '28',
+        '29',
+        '30',
+        '31',
+		
+	]
+
+	$: console.log('Changed selected:', selected1)
+	$: console.log('Updated options:', options1)
+
+
+
 </script>
 
 {#if isOpen}
@@ -41,245 +126,32 @@
 				<div class="modal-date-birth w-full flex mt-3 text-center">
 					<label for="">Date of birth</label>
 
-					<div class="modal-date-alert w-4 h-4 inline-block text-xs text-center text-white ml-1 rounded-[50%]">
+					<div class="modal-date-alert w-4 h-4 inline-block text-xs text-center text-white ml-1 rounded-[50%] ">
 						<a>&#63;</a>
 					</div>
 				</div>
-
+                
 				<div class="modal-date-selection w-[100%] flex  mt-[5px]">
 					<div class="select-choice w-[33%]">
-						<select name="" id="" class="w-[96%] p-[5px] text-[17px] rounded-[5px] border-none text-black">
-							<option value=""> 1 </option>
-							<option value=""> 2 </option>
-							<option value=""> 3 </option>
-							<option value=""> 4 </option>
-
-							<option value=""> 5 </option>
-
-							<option value=""> 6 </option>
-
-							<option value=""> 7 </option>
-
-							<option value=""> 8 </option>
-
-							<option value=""> 9 </option>
-
-							<option value=""> 10 </option>
-
-							<option value=""> 11 </option>
-
-							<option value=""> 12 </option>
-
-							<option value=""> 13 </option>
-
-							<option value=""> 14 </option>
-
-							<option value=""> 15 </option>
-
-							<option value=""> 16 </option>
-
-							<option value=""> 17 </option>
-
-							<option value=""> 18 </option>
-
-							<option value=""> 19 </option>
-
-							<option value=""> 20 </option>
-
-							<option value=""> 21 </option>
-
-							<option value=""> 22 </option>
-
-							<option value=""> 23 </option>
-
-							<option value=""> 24 </option>
-
-							<option value=""> 25 </option>
-
-							<option value=""> 26 </option>
-
-							<option value=""> 27 </option>
-
-							<option value=""> 28 </option>
-
-							<option value=""> 29 </option>
-
-							<option value=""> 30 </option>
-
-							<option value=""> 31 </option>
+                        <select name="" id="" bind:value={selected2} class="w-[96%] p-[5px] text-[17px] rounded-[5px] border-none text-black">
+                           
+                            {#each options2 as value}<option {value}>{value}</option>{/each}
+                        </select>
+					</div>
+					<div class="select-choice w-[33%]">
+					
+							<select name="" id="" bind:value={selected1} class="w-[96%] p-[5px] text-[17px] rounded-[5px] border-none text-black">
+                           
+                                {#each options1 as value}<option {value}>{value}</option>{/each}
+                            
 						</select>
 					</div>
 					<div class="select-choice w-[33%]">
-						<select name="" id="" class="w-[96%] p-[5px] text-[17px] rounded-[5px] border-none text-black">
-							<option value=""> Jan </option>
-
-							<option value=""> Feb </option>
-
-							<option value=""> Mar </option>
-
-							<option value=""> Apr </option>
-
-							<option value=""> May </option>
-
-							<option value=""> Jun </option>
-
-							<option value=""> Jul </option>
-
-							<option value=""> Aug </option>
-
-							<option value=""> Sep </option>
-
-							<option value=""> Auc </option>
-
-							<option value=""> Nav </option>
-
-							<option value=""> Dec </option>
-						</select>
-					</div>
-					<div class="select-choice w-[33%]">
-						<select name="" id="" class="w-[96%] p-[5px] text-[17px] rounded-[5px] border-none text-black">
-							<option value=""> 1950 </option>
-
-							<option value=""> 1951 </option>
-
-							<option value=""> 1952 </option>
-
-							<option value=""> 1953 </option>
-
-							<option value=""> 1954 </option>
-
-							<option value=""> 1955 </option>
-
-							<option value=""> 1956 </option>
-
-							<option value=""> 1957 </option>
-
-							<option value=""> 1958 </option>
-
-							<option value=""> 1959 </option>
-
-							<option value=""> 1960 </option>
-
-							<option value=""> 1961 </option>
-
-							<option value=""> 1962 </option>
-
-							<option value=""> 1963 </option>
-
-							<option value=""> 1964 </option>
-
-							<option value=""> 1965 </option>
-
-							<option value=""> 1966 </option>
-
-							<option value=""> 1967 </option>
-
-							<option value=""> 1968 </option>
-
-							<option value=""> 1969 </option>
-
-							<option value=""> 1970 </option>
-
-							<option value=""> 1971 </option>
-
-							<option value=""> 1972 </option>
-
-							<option value=""> 1973 </option>
-
-							<option value=""> 1974 </option>
-
-							<option value=""> 1975 </option>
-
-							<option value=""> 1976 </option>
-
-							<option value=""> 1977 </option>
-
-							<option value=""> 1978 </option>
-
-							<option value=""> 1979 </option>
-
-							<option value=""> 1980 </option>
-
-							<option value=""> 1981 </option>
-
-							<option value=""> 1982 </option>
-
-							<option value=""> 1983 </option>
-
-							<option value=""> 1984 </option>
-
-							<option value=""> 1985 </option>
-
-							<option value=""> 1986 </option>
-
-							<option value=""> 1987 </option>
-
-							<option value=""> 1988 </option>
-
-							<option value=""> 1989 </option>
-
-							<option value=""> 1990 </option>
-
-							<option value=""> 1991 </option>
-
-							<option value=""> 1992 </option>
-
-							<option value=""> 1993 </option>
-
-							<option value=""> 1994 </option>
-
-							<option value=""> 1995 </option>
-
-							<option value=""> 1996 </option>
-
-							<option value=""> 1997 </option>
-
-							<option value=""> 1998 </option>
-
-							<option value=""> 1999 </option>
-
-							<option value=""> 2000 </option>
-
-							<option value=""> 2001 </option>
-
-							<option value=""> 2002 </option>
-
-							<option value=""> 2003 </option>
-
-							<option value=""> 2004 </option>
-
-							<option value=""> 2005 </option>
-
-							<option value=""> 2006 </option>
-
-							<option value=""> 2007 </option>
-
-							<option value=""> 2008 </option>
-
-							<option value=""> 2009 </option>
-
-							<option value=""> 2010 </option>
-
-							<option value=""> 2011 </option>
-
-							<option value=""> 2012 </option>
-
-							<option value=""> 2013 </option>
-
-							<option value=""> 2014 </option>
-
-							<option value=""> 2015 </option>
-
-							<option value=""> 2016 </option>
-
-							<option value=""> 2017 </option>
-
-							<option value=""> 2018 </option>
-
-							<option value=""> 2019 </option>
-
-							<option value=""> 2020 </option>
-						</select>
+						<select name="" id="" bind:value={selected} class="w-[96%] p-[5px] text-[17px] rounded-[5px] border-none text-black">
+                           
+                                {#each options as value}<option {value}>{value}</option>{/each}
+                            </select>
+							
 					</div>
 				</div>
 
